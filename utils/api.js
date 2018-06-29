61dbdba88fb663083ddcf8f16d34556cef8c48b2
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-06-29 05:32:48 +0200

    Use env variables for CMS credentials

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-  space: `n2fxef4hydn9`,
-  accessToken: `14aee5a3c3d231330df1e1c67927059f2280669e615184b69b7eaba4f0aed0ac`,
+  space: process.env.API_SPACE,
+  accessToken: process.env.API_TOKEN,
 });
